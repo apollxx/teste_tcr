be693e696ac219bdf0a6989d7b1ffb4a8f89fa3b
lezera12@gmail.com <L$e$ozera@12@>
2024-11-08 14:28:12 -0300

    ok

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:TCR/main.dart';
+import 'package:tcr/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
