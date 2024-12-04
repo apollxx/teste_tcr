32408f6660d37b165e3d7e22029ad01c0bfe57bc
lezera12@gmail.com <L$e$ozera@12@>
2024-12-04 14:28:18 -0300

    ok

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.mycompany.tcrteste
 
+import android.os.Bundle
+import android.webkit.CookieManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
